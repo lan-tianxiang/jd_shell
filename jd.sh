d713cac357bdd55d6745cfea966e4784ebe2057c
lan-tianxiang <42911023+lan-tianxiang@users.noreply.github.com>
2021-03-29 00:53:32 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -288,8 +288,8 @@ function Run_Normal {
 
   if [ AutoHelpme = true ]; then
     if [ -f ${LogDir}/export_sharecodes/export_sharecodes.log ]; then
-      cp -f FileConf ${ConfigDir}/config.sh.temp
-      echo LogDir/export_sharecodes/export_sharecodes.log >> config.sh.temp
+      cp -f ${FileConf} ${ConfigDir}/config.sh.temp
+      echo ${LogDir}/export_sharecodes/export_sharecodes.log >> config.sh.temp
       FileConf=${ConfigDir}/config.sh.temp
       Import_Conf && Detect_Cron && Set_Env
     else
