14b9ec0d8289b6e2074e695cde13d31c6f8cf75f
lan-tianxiang <42911023+lan-tianxiang@users.noreply.github.com>
2021-04-01 21:51:42 +0800

    新增京东手机狂欢城，自动助力！！！！！

--- export_sharecodes.sh
+++ export_sharecodes.sh
@@ -13,7 +13,7 @@ Tips="从日志中未找到任何互助码"
 
 ## 所有有互助码的活动，只需要把脚本名称去掉前缀 jd_ 后列在 Name1 中，将其中文名称列在 Name2 中，对应 config.sh 中互助码后缀列在 Name3 中即可。
 ## Name1、Name2 和 Name3 中的三个名称必须一一对应。
-Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc bookshop cash sgmh cfd global arnivalcity)
+Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jdzz jxnc bookshop cash sgmh cfd global carnivalcity)
 Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京东赚赚 京喜农场 口袋书店 签到领现金 闪购盲盒 京喜财富岛 环球挑战赛 京东手机狂欢城)
 Name3=(Fruit Pet Bean DreamFactory JdFactory Joy Jdzz Jxnc BookShop Cash Sgmh Cfd Global Carnivalcity)
 
